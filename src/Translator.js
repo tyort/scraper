b7d393b0307389cce50ec161fcf2b626832e86cc
tyort <neotyo1986@gmail.com>
2023-05-01 14:01:13 +0300

    api google переводчика

--- src/Translator.js
+++ src/Translator.js
@@ -1,3 +1,16 @@
-export default class Translator {
-  
+const axios = require('axios');
+class Translator {
+  constructor({original, translation}) {
+    this.url = `https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=${original}&tl=${translation}&q=`;
+  };
+
+  async getTranslation(content) {
+    const totalUrl = `${this.url}${encodeURI(content)}`;
+    const translatedContent = await axios(totalUrl);
+    return JSON.parse(translatedContent.data[0][0][0]);
+  };
+}
+
+module.exports = {
+  Translator
 }
