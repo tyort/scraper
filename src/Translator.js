b7bbca830c9043d940e5b12924a7b554eea4bda4
tyort <neotyo1986@gmail.com>
2023-05-02 23:29:06 +0300

    Парсим JSON

--- src/Translator.js
+++ src/Translator.js
@@ -7,8 +7,8 @@ class Translator {
   async getTranslation(content) {
     const totalUrl = `${this.url}${encodeURI(content)}`;
     const response = await axios(totalUrl);
-    // const translation = JSON.parse(response.data[0][0][0]);
-    return response.data[0][0][0];
+    const translation = JSON.parse(response.data[0][0][0]);
+    return translation;
   };
 }
 
