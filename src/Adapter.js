6bb230a2a02539e17b182118b63c5e8986eb1492
tyort <neotyo1986@gmail.com>
2023-05-02 23:28:47 +0300

    Сщврешенствуем адаптер

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,20 +1,22 @@
 class Adapter {
-  getContent(content) {
-    const title = content('.ReviewDetail_area_car__vLgIZ.fs16.ReviewDetail_report__cdNg8').text();
-    const publicationDate = content('.ReviewDetail_time__2zU0I.fs14.fcBlack3').text();
-    const highestPrice = content('.ReviewDetail_price__CeeuP.fs14.fcBlack3').text();
+  getUntranslatableContent(content) {
     const pics = [];
-    const features = [];
-    const addFeatures = [];
-    const options = [];
     content('.PhotoSwipe_list__AsRzI button img')
       .each(function(index, image) {
         const srcData = content(image).attr('src');
-        pics.push({ [`pic${index + 1}`]: srcData })
+        pics.push(srcData);
       });
+    return pics;
+  }
+
+  getContent(content) {
+    const features = [];
+    const addFeatures = [];
+    const options = [];
+    const dealerIndexes = [];
     const sellerComment = content('.ReviewDetail_area_review_seller__o17ek.fs15.fcBlack2').text();
-    const newDealer = content('.ReviewDetail_review_chat__3246N.fcBlack2').text();
-    const name = content('.CarInfo_name__yMacL.fs16.fwBold').text();
+    const comment = content('.ReviewDetail_review_chat__3246N.fcBlack2').text();
+    const vehicleName = content('.CarInfo_name__yMacL.fs16.fwBold').text();
     content('.CarInfo_info__dlTdp.fs16.fcBlack2 .CarInfo_item__hBVr-')
       .each(function(_index, feature) {
         const text = content(feature).text();
@@ -29,34 +31,40 @@ class Adapter {
       .each(function(_index, option) {
         const text = content(option).text();
         options.push(text);
-      })
-    const bestCost = content('[data-testid="max"]').text();
-    const averageCost = content('[data-testid="min"]').text();
-    const bidders = content('[data-testid="dealerCnt"]').text();
-    
+      });
+    content('.DealerInfo_sell_info__SdM1h.fs14.fcBlack2 li span')
+      .each(function(_index, item) {
+        const text = content(item).text();
+        dealerIndexes.push(text);
+      });
 
     const result = {
-      title,
-      publicationDate,
-      highestPrice,
-      pics,
-      sellerComment: sellerComment.replace(/\n/gmi, ' '),
-      newDealer,
-      vehicle: {
-        mainInfo: {
-          name: name.replace(/\n/gmi, ' '),
-          features
+      "title": content('.ReviewDetail_area_car__vLgIZ.fs16.ReviewDetail_report__cdNg8').text(),
+      "publicationDate": content('.ReviewDetail_time__2zU0I.fs14.fcBlack3').text(),
+      "highestPrice": content('.ReviewDetail_price__CeeuP.fs14.fcBlack3').text(),
+      "sellerComment": sellerComment.replace(/\n/gmi, ' '),
+      "newDealer": {
+        "dealerName": content('[data-testid="dealerInfoName"]').text(),
+        "dalerCompany": content('.DealerInfo_company__r66ob.fs15.fcBlack2').text(),
+        "winningBets": dealerIndexes[0],
+        "dealerRate": dealerIndexes[1],
+        "ratePer": content('.DealerInfo_sel_months__z6dPu.fs14.fcBlack3').text()
+      },
+      "vehicle": {
+        "mainInfo": {
+          "title": vehicleName.replace(/\n/gmi, ' '),
+          "features": features
         },
-        addInfo: {
-          color: addFeatures[0],
-          exchangeBuy: addFeatures[1],
-          rentHistory: addFeatures[2]
+        "addInfo": {
+          "vehicleColor": addFeatures[0],
+          "exchangeBuy": addFeatures[1],
+          "rentHist": addFeatures[2]
         },
-        options,
-        costRatio: {
-          bestCost,
-          averageCost,
-          bidders
+        "optionsList": options,
+        "costRatio": {
+          "bestCost": content('[data-testid="max"]').text(),
+          "averageCost": content('[data-testid="min"]').text(),
+          "biddersAmount": content('[data-testid="dealerCnt"]').text()
         }
       }
     }
