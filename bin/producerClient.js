691456a6ceb5499d08401a987d118e618cb385cd
tyort <neotyo1986@gmail.com>
2023-05-13 13:11:57 +0300

    Создаем класс NatsService

--- bin/producerClient.js
+++ bin/producerClient.js
@@ -1,95 +1,24 @@
-import pkg from 'nats';
-const { connect, Empty, StringCodec, AckPolicy } = pkg;
 import { VEHICLE_URL } from '../src/const.js';
+import NatsService from '../src/services/NatsService.js';
 
 const STREAM_NAME = 'encarStream';
 const SUBJECT = 'encarSubj.*';
-const DURABLE_NAME = 'encarDataConsumer';
-const SC = StringCodec();
-let nc;
-let jsm;
+
+const natsService = new NatsService();
 
 async function main() {
   try {
-    nc = await connect({
-      name: 'tyort-connection',
-      verbose: true,
-      // reconnect: false,
-      reconnectTimeWait: 10 * 1000,
-      maxReconnectAttempts: 10,
-      // pingInterval: 20 * 1000,
-      servers: [
-        `${process.env.NATS_SERVER_HOST}:${process.env.NATS_SERVER_PORT}`,
-      ],
-      // token: process.env.TOKEN,
-      // debug: true,
-    });
-
-    jsm = await nc.jetstreamManager();
-    await jsm.streams.add({ name: STREAM_NAME, subjects: [SUBJECT] });
-
-    const jsClient = await nc.jetstream();
-
-    await jsClient.publish('encarSubj.unit', SC.encode(VEHICLE_URL), {
-      msgID: VEHICLE_URL.split('id=')[1],
-      expect: { streamName: STREAM_NAME },
-    });
-
-    // add a new durable pull consumer
-    await jsm.consumers.add(STREAM_NAME, {
-      durable_name: DURABLE_NAME,
-      ack_policy: AckPolicy.Explicit,
-      filter_subject: 'encarSubj.unit',
-    });
+    await natsService.connect('producer-connection');
+    await natsService.setJsm();
+    await natsService.addStream(STREAM_NAME, SUBJECT);
+    await natsService.setJsc();
+    await natsService.publish(STREAM_NAME, 'encarSubj.unit', VEHICLE_URL);
   } catch (err) {
     console.log(err);
   } finally {
-    // await jsm.streams.delete(STREAM_NAME);
-    // await nc.drain();
+    // await natsService.deleteStream(STREAM_NAME);
+    await natsService.connection.drain();
   }
 }
 
 main().then();
-
-// // you can find out where you connected:
-// console.log(`connected to a nats server version ${nc.info.version}`);
-
-// // or information about the data in/out of the client:
-// const stats = nc.stats();
-// console.log(
-//   `client sent ${stats.outMsgs} messages and received ${stats.inMsgs}`
-// );
-
-// nc.closed().then(() => {
-//   console.log('the connection closed!');
-// });
-
-// (async () => {
-//   for await (const s of nc.status()) {
-//     switch (s.type) {
-//       case Status.Disconnect:
-//         console.log(`client disconnected - ${s.data}`);
-//         break;
-//       case Status.LDM:
-//         console.log('client has been requested to reconnect');
-//         break;
-//       case Status.Update:
-//         console.log(`client received a cluster update - ${s.data}`);
-//         break;
-//       case Status.Reconnect:
-//         console.log(`client reconnected - ${s.data}`);
-//         break;
-//       case Status.Error:
-//         console.log('client got a permissions error');
-//         break;
-//       case DebugEvents.Reconnecting:
-//         console.log('client is attempting to reconnect');
-//         break;
-//       case DebugEvents.StaleConnection:
-//         console.log('client has a stale connection');
-//         break;
-//       default:
-//         console.log(`got an unknown status ${s.type}`);
-//     }
-//   }
-// })().then();
---
+++ src/services/NatsService.js
@@ -0,0 +1,79 @@
+import pkg from 'nats';
+const { connect, StringCodec } = pkg;
+
+class NatsService {
+  constructor() {
+    this.connection = null;
+    this.jsm = null;
+    this.jsc = null;
+  }
+
+  async connect(name) {
+    this.connection = await connect({
+      name,
+      verbose: true,
+      reconnect: false,
+      reconnectTimeWait: 10 * 1000,
+      maxReconnectAttempts: 10,
+      pingInterval: 20 * 1000,
+      servers: [
+        `${process.env.NATS_SERVER_HOST}:${process.env.NATS_SERVER_PORT}`,
+      ],
+      token: process.env.TOKEN,
+      // debug: true,
+    });
+    return this.connection;
+  }
+
+  async setJsm() {
+    if (!this.connection) {
+      return;
+    }
+    this.jsm = await this.connection.jetstreamManager();
+  }
+
+  async setJsc() {
+    if (!this.connection) {
+      return;
+    }
+    this.jsc = await this.connection.jetstream();
+  }
+
+  async publish(streamName, subj, message) {
+    if (!this.jsc) {
+      return;
+    }
+
+    await this.jsc.publish(subj, StringCodec().encode(message), {
+      msgID: message.split('id=')[1],
+      expect: { streamName: streamName },
+    });
+  }
+
+  async findStream(streamName) {
+    if (!this.jsm) {
+      return;
+    }
+    try {
+      return await this.jsm.streams.info(streamName);
+    } catch (err) {
+      return err.message === 'stream not found' && null;
+    }
+  }
+
+  async addStream(streamName, subject) {
+    const currentStream = await this.findStream(streamName);
+    if (!currentStream && this.jsm) {
+      await this.jsm.streams.add({ name: streamName, subjects: [subject] });
+    }
+  }
+
+  async deleteStream(streamName) {
+    const currentStream = await this.findStream(streamName);
+    if (currentStream && this.jsm) {
+      await this.jsm.streams.delete(streamName);
+    }
+  }
+}
+
+export default NatsService;
